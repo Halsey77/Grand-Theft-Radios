91f077075957c58f91918b0724dee11e0d839df7
Halsey77 <provcc143300@gmail.com>
2023-12-23 20:17:37 +0700

    set stations spinner when game changes

--- app/src/main/java/com/halsey/grandtheftradios/MainActivity.kt
+++ app/src/main/java/com/halsey/grandtheftradios/MainActivity.kt
@@ -2,6 +2,8 @@ package com.halsey.grandtheftradios
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
@@ -16,6 +18,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //initialize variables
+        gameSpinner = findViewById(R.id.gameSpinner)
+        stationSpinner = findViewById(R.id.stationSpinner)
+        gameText = findViewById(R.id.gameText)
+        stationText = findViewById(R.id.stationText)
+
+        //initialize the activity
         initialize()
     }
 
@@ -24,7 +34,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setGameSpinner() {
-        gameSpinner = findViewById(R.id.gameSpinner)
         ArrayAdapter.createFromResource(
             this,
             R.array.gta_games,
@@ -32,6 +41,36 @@ class MainActivity : AppCompatActivity() {
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             gameSpinner.adapter = adapter
+        }
+
+        gameSpinner.setSelection(0)
+
+        //make station spinner change when game spinner changes
+        gameSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View, position: Int, id: Long
+            ) {
+                //set station spinner to the correct stations
+                setStationSpinner(position);
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                //nothing happens
+            }
+        }
+    }
+
+    private fun setStationSpinner(position: Int) {
+        val stations = resources.getStringArray(R.array.gta_stations)[position].split(';').toTypedArray();
+
+        ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            stations
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            stationSpinner.adapter = adapter
         }
     }
 }
---
+++ app/src/main/java/com/halsey/grandtheftradios/Util.java
@@ -0,0 +1,5 @@
+package com.halsey.grandtheftradios;
+
+public class Util {
+
+}
