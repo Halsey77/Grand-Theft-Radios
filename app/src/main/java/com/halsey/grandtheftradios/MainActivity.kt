22a65ac0522cc7629f7c3642ca4983c629e7fd84
Halsey77 <provcc143300@gmail.com>
2023-12-23 19:53:58 +0700

    set game spinner

--- app/src/main/java/com/halsey/grandtheftradios/MainActivity.kt
+++ app/src/main/java/com/halsey/grandtheftradios/MainActivity.kt
@@ -2,10 +2,36 @@ package com.halsey.grandtheftradios
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var gameSpinner: Spinner
+    private lateinit var stationSpinner: Spinner
+    private lateinit var gameText: TextView
+    private lateinit var stationText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialize()
+    }
+
+    private fun initialize() {
+        setGameSpinner()
+    }
+
+    private fun setGameSpinner() {
+        gameSpinner = findViewById(R.id.gameSpinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.gta_games,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            gameSpinner.adapter = adapter
+        }
     }
 }
